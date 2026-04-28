ed43e404f90c15b7a5863ad657ddc082b3dd4fe5
kstocks3 <kstocks3@charlotte.edu>
2026-04-28 10:03:56 -0400

    Update Category.java

--- src/java/domain/Category.java
+++ src/java/domain/Category.java
@@ -1,4 +1,11 @@
 package domain;
 
 public class Category {
+    private ClothingType type;
+    private String description;
+
+    public Category(ClothingType type, String description){
+        this.type = type;
+        this.description = description;
+    }
 }
