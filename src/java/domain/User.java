9e5f1f67beadac41b8072b9b8420c93bd8f8713c
kstocks3 <kstocks3@charlotte.edu>
2026-04-28 09:57:36 -0400

    Update User.java

--- src/java/domain/User.java
+++ src/java/domain/User.java
@@ -1,4 +1,21 @@
 package domain;
 
 public abstract class User {
+      private int id;
+    private String name;
+    private List<ClothingItem> wardrobe;
+    private List<List<ClothingItem>> savedOutfits;
+
+    public User(int id, String name){
+        this.id = id;
+        this.name = name;
+        this.wardrobe = new ArrayList<>();
+        this.savedOutfits = new ArrayList<>();
+    }
+
+    public List<ClothingItem> getWardrobe() {
+        return wardrobe;
+    }
+    public void addClothingItem(ClothingItem item){
+        this.wardrobe.add(item);
 }
