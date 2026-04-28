3e31ce685a6926b01e0cb4739e694ace8a0f74ff
kstocks3 <kstocks3@charlotte.edu>
2026-04-28 10:03:15 -0400

    Update ClothingItem.java

--- src/java/domain/ClothingItem.java
+++ src/java/domain/ClothingItem.java
@@ -1,4 +1,31 @@
 package domain;
 
 public abstract class ClothingItem {
+  private int id;
+    private String name;
+    private String imageFilePath;
+    private String color;
+    private Boolean favorite;
+    private int wearCount;
+    private List<String> tags = new ArrayList<>();
+    private ClothingType category;
+    private Season season;
+    private WearCounter counter = new WearCounter();
+
+    public String getName() {
+        return name;
+    }
+
+    public void incrementWearCount() {
+        this.wearCount++;
+        counter.incrementWear();
+    }
+
+    public void addTag(String tag){
+        tags.add(tag);
+    }
+
+    public void removeTag(String tag){
+        tags.remove(tag);
+    }
 }
