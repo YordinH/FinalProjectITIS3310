3fd47cb0ab88f8e8a5dcdc1ba5414d5a0f61b43d
Wes Packham <wesleypackham@gmail.com>
2026-04-29 12:47:02 -0400

    feat: Add a count field, incrementWear(), and getCount() to WearCounter.java

--- src/java/domain/WearCounter.java
+++ src/java/domain/WearCounter.java
@@ -1,4 +1,13 @@
 package domain;
 
 public class WearCounter {
+    private int count;
+
+    public void incrementWear() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
