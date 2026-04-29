25c352322882bbb2f4f94fa563c37e851cc2808f
Yordin Hasty <yhasty@charlotte.edu>
2026-04-29 19:21:22 -0400

    Add Client.java methods
    Added client methods based off UML

--- src/java/domain/Client.java
+++ src/java/domain/Client.java
@@ -1,4 +1,22 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Client extends User {
+    private List<Outfit> ownedOutfits;
+
+    public Client(int id, String name) {
+        super(id, name);
+        this.ownedOutfits = new ArrayList<>();
+    }
+
+    @Override
+    public String getRole() {
+        return "Client";
+    }
+
+    public List<Outfit> getOwnedOutfits() {
+        return ownedOutfits;
+    }
 }
