298ad1ae79b5d0f1de06405493f5f9eae1d984d9
Wes Packham <wesleypackham@gmail.com>
2026-04-29 12:49:21 -0400

    User.java: added import statements, fixed syntax and indentation. Added getId() getName() getRole()

--- src/java/domain/User.java
+++ src/java/domain/User.java
@@ -1,21 +1,36 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class User {
-      private int id;
+    private int id;
     private String name;
     private List<ClothingItem> wardrobe;
     private List<List<ClothingItem>> savedOutfits;
 
-    public User(int id, String name){
+    public User(int id, String name) {
         this.id = id;
         this.name = name;
         this.wardrobe = new ArrayList<>();
         this.savedOutfits = new ArrayList<>();
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract String getRole();
+
     public List<ClothingItem> getWardrobe() {
         return wardrobe;
     }
-    public void addClothingItem(ClothingItem item){
-        this.wardrobe.add(item);
+
+    public void addClothingItem(ClothingItem item) {
+        wardrobe.add(item);
+    }
 }
