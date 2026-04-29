4b99c605c0530d76c796cd75775c9ec6c0efe518
Yordin Hasty <yhasty@charlotte.edu>
2026-04-29 19:29:15 -0400

    Add methods to ClothingRepository.java
    Created all methods for ClothingRepository, Need to code in load/persist with disk.

--- src/java/repositories/ClothingRepository.java
+++ src/java/repositories/ClothingRepository.java
@@ -1,6 +1,52 @@
 package repositories;
 
 import contracts.IClothingRepository;
+import contracts.IFileService;
+import domain.ClothingItem;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClothingRepository implements IClothingRepository {
+    private static ClothingRepository instance;
+    private Map<Integer, ClothingItem> items;
+    private IFileService fileService;
+
+    private ClothingRepository() {
+        this.items = new HashMap<>();
+    }
+
+    public static ClothingRepository getInstance() {
+        if (instance == null) {
+            instance = new ClothingRepository();
+        }
+        return instance;
+    }
+
+    @Override
+    public void save(ClothingItem item) {
+        items.put(item.getId(), item);
+    }
+
+    @Override
+    public void remove(int itemId) {
+        items.remove(itemId);
+    }
+
+    @Override
+    public ClothingItem getById(int itemId) {
+        return items.get(itemId);
+    }
+
+    @Override
+    public List<ClothingItem> getAll() {
+        return new ArrayList<>(items.values());
+    }
+
+    public void loadFromDisk() {
+    }
+
+    public void persistToDisk() {
+    }
 }
