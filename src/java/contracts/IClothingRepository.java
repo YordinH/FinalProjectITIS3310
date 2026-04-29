2e571ee5dd21e8102c0cdefaed18652bec6b9fad
Yordin Hasty <yhasty@charlotte.edu>
2026-04-29 19:29:42 -0400

    Create interface methods for IClothingRepository
    Created all methods for IClothingRepository

--- src/java/contracts/IClothingRepository.java
+++ src/java/contracts/IClothingRepository.java
@@ -1,4 +1,11 @@
 package contracts;
 
+import domain.ClothingItem;
+import java.util.List;
+
 public interface IClothingRepository {
+    void save(ClothingItem item);
+    void remove(int itemId);
+    ClothingItem getById(int itemId);
+    List<ClothingItem> getAll();
 }
