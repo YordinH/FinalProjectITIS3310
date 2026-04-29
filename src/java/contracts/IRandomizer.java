832cff46dd32ec2eeb3e309e8b4b2d3d9d06d673
Yordin Hasty <yhasty@charlotte.edu>
2026-04-29 19:30:16 -0400

    Implement IRandomizer.java
    Added generate() to IRandomizer with 3 arguments.

--- src/java/contracts/IRandomizer.java
+++ src/java/contracts/IRandomizer.java
@@ -1,4 +1,10 @@
 package contracts;
 
+import domain.ClothingItem;
+import domain.ClothingType;
+import domain.Outfit;
+import java.util.List;
+
 public interface IRandomizer {
+    Outfit generate(List<ClothingItem> pool, List<ClothingItem> locked, List<ClothingType> disabled);
 }
