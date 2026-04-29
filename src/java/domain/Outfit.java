1543d16223d87ba2a5e9e63d089e84bde507f1de
Yordin Hasty <yhasty@charlotte.edu>
2026-04-29 19:20:25 -0400

    Add Methods to Outfit.java
    Added all required methods from UML into Outfit.java

--- src/java/domain/Outfit.java
+++ src/java/domain/Outfit.java
@@ -1,4 +1,50 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Outfit {
+    private int id;
+    private String name;
+    private boolean favorite;
+    private int wearCount;
+    private List<String> tags;
+    private List<ClothingItem> items;
+
+    public Outfit(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.favorite = false;
+        this.wearCount = 0;
+        this.tags = new ArrayList<>();
+        this.items = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<ClothingItem> getItems() {
+        return items;
+    }
+
+    public void addItem(ClothingItem item) {
+        items.add(item);
+    }
+
+    public void removeItem(ClothingItem item) {
+        items.remove(item);
+    }
+
+    public void markFavorite() {
+        favorite = true;
+    }
+
+    public void incrementWearCount() {
+        wearCount++;
+    }
 }
