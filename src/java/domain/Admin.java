132d8ff5d3e11e546e9fcd3cd04116f184e528c4
Yordin Hasty <yhasty@charlotte.edu>
2026-04-29 19:20:59 -0400

    Add methods to Admin.java
    Added constructor and getRole() method to admin.

--- src/java/domain/Admin.java
+++ src/java/domain/Admin.java
@@ -1,4 +1,12 @@
 package domain;
 
 public class Admin extends User {
+    public Admin(int id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public String getRole() {
+        return "Admin";
+    }
 }
