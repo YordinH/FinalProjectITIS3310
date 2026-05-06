ab7ebdaa11676c09e81f4022216997450a6ab4a8
Wes Packham <wesleypackham@gmail.com>
2026-05-06 16:49:08 -0400

    feat: Add admin delete-client, outfit deletion,relative image paths, updated UML

--- src/java/Program.java
+++ src/java/Program.java
@@ -386,7 +386,7 @@ public class Program {
 
             ClothingItem item = ClothingFactory.create(
                     clothingType, nextId++, currentUser.getId(), name,
-                    processed.getAbsolutePath(), color, season, subtypeAttr);
+                    processed.getPath(), color, season, subtypeAttr);
             repo.save(item);
             fileService.markImported(file.getName());
             imageViewer.close();
